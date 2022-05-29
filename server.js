3db795145b084dc252f0332adfabfbf8f5b36141
BayDeveloper <31515986+BayDeveloper@users.noreply.github.com>
2022-05-29 16:34:26 +0700

    fix

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-"use strict";
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const expect = require("chai").expect;
 const cors = require("cors");
 require("dotenv").config();
 
 const apiRoutes = require("./routes/api.js");
 const fccTestingRoutes = require("./routes/fcctesting.js");
 const runner = require("./test-runner");
-
-//const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require("./middleware/error-handler");
 
 let app = express();
 
